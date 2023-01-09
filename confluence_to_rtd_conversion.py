8d9407f21f9bf81cbd9b40f078fe882bbb9d4964
HarveyBeynon <hbeynon@aurrigo.com>
2023-01-09 16:58:08 +0000

    Refactored code

--- confluence_to_rtd_conversion.py
+++ confluence_to_rtd_conversion.py
@@ -1,18 +1,4 @@
-# This is a python script that aims to convert the documentation from 
-# confluence to read the docs
-
-# Process of convesion
-# 1. Export the confluenece page to word (.doc)
-# 2. Convert file from .doc to .docx (needed for step 3)
-# 3. Call pandoc to convert the .docx to .rst (pandoc oldfilename.docx -o newfilename.rst)
-# 4. Add the new page to the correct location in the "docs" folder
-# 5. Add a refrence to thenew file in the index.rst's toc
-# 6. Git add > commit > push to allow read the docs to display the new repo content
-
-# Link address to convert page to word https://aurrigo.atlassian.net/wiki/exportword?pageId=10780673
-
 #pip install atlassian-python-api
-
 #pip install pypiwin32
 
 import os.path
@@ -26,8 +12,9 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-# subprocess.run(["curl", "-D-", "-X", "GET", "-H", "Authorization: Basic aGFydmV5aWJleW5vbkBnbWFpbC5jb206a0VuNUFQUEk5ZWhUbDVsZkhSalo1MTM1", "Content-Type: application/json", "-H", "https://harveybeynon.atlassian.net/wiki/exportword?pageId=491526", "--output", "exportedDocs/apitest.doc"])
-
+# TODO Cycle through each confluence page that is to be published to read the docs and get their ID nums.
+# TODO create a loop that exports each requested page (cycle the page - id)
+# Export Confluence file as .doc
 subprocess.run(["curl", "-D-", "-X", "GET", "-H", f"Authorization: Basic {API_KEY}", "-H", "Content-Type: application/json", "https://harveybeynon.atlassian.net/wiki/exportword?pageId=491526", "--output", "exportedDocs/testAPI.doc"])
 
 # curl -D- \
@@ -82,10 +69,10 @@ for item in test:
         dst_path = os.path.join("docs/pages/", item)
         shutil.move(src_path, dst_path)
 
-# # TODO currently this scripts is called from a git bash shell - May need to get the git shh and secret key
-# # if this script were to run from a an API call.
-# # Commit and push to GitHub
-# subprocess.run(["git", "add", "."])
-# subprocess.run(["git", "commit", "-m", "'commit from python script'"])
-# subprocess.run(["git", "push"])
-# print("Convesion finished - new docs should now be viewable on Read the Docs")
+# TODO currently this scripts is called from a git bash shell - May need to get the git shh and secret key
+# if this script were to run from a an API call.
+# Commit and push to GitHub
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "-m", "'commit from python script'"])
+subprocess.run(["git", "push"])
+print("Convesion finished - new docs should now be viewable on Read the Docs")
