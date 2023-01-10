566b3ce83c36b8b0441102d7fba43dbb8c22f7d2
HarveyBeynon <hbeynon@aurrigo.com>
2023-01-10 09:30:50 +0000

    Added functions to delete and move files

--- confluence_to_rtd_conversion.py
+++ confluence_to_rtd_conversion.py
@@ -1,6 +1,3 @@
-#pip install atlassian-python-api
-#pip install pypiwin32
-
 import os.path
 import win32com.client
 import subprocess
@@ -12,28 +9,34 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
+# Delete all files in a given directory
+def delete_files_in_dir(dir_name, extension):
+    currentDir = os.listdir(dir_name)
+    for item in currentDir:
+        if (item.endswith(extension)):
+            os.remove(os.path.join(dir_name, item))
+
+# Move all file with extention from on dir to another
+def move_files(src_dir, dst_dir, extension):
+    test = os.listdir(src_dir)
+    for item in test:
+        if (item.endswith(extension)):
+            src_path = os.path.join(src_dir, item)
+            dst_path = os.path.join(dst_dir, item)
+            shutil.move(src_path, dst_path)
+
+delete_files_in_dir("exportedDocs/", ".doc")
+delete_files_in_dir("docs/pages/", ".rst")
+
 # TODO Cycle through each confluence page that is to be published to read the docs and get their ID nums.
 
-
-# pageIds = ["229377", "851969", "491526"] # Hardcoding page Ids into the array
-# for page in pageIds:
-#     print(page)
-#     subprocess.run(["curl", "-D-", "-X", "GET", "-H", f"Authorization: Basic {API_KEY}", "-H", "Content-Type: application/json", f"https://harveybeynon.atlassian.net/wiki/exportword?pageId={page}", "--output", f"exportedDocs/{page}.doc"]) # Need to find file names
-
-pageIds = {"Lorem+Ipsum":"229377", "Lorem+Ipsum+Example":"851969", "API+Page":"491526"} # Hardcoding pageIds and Filenames
+# Exporting confluence page as .doc
+# NOTE - Read the Docs lists the files alphabetically, hence it will be good paractise to number the files
+pageIds = {"Lorem+Ipsum":"229377", "Lorem+Ipsum+Example":"851969", "API+Page":"491526"} # Hardcoding pageIds and Filenames - NOTE these file need to be in order.
+pageNum = 1
 for key, value in pageIds.items():
-    print(key, value)
-    subprocess.run(["curl", "-D-", "-X", "GET", "-H", f"Authorization: Basic {API_KEY}", "-H", "Content-Type: application/json", f"https://harveybeynon.atlassian.net/wiki/exportword?pageId={value}", "--output", f"exportedDocs/{key}.doc"])
-
-# Export Confluence file as .doc
-# subprocess.run(["curl", "-D-", "-X", "GET", "-H", f"Authorization: Basic {API_KEY}", "-H", "Content-Type: application/json", "https://harveybeynon.atlassian.net/wiki/exportword?pageId=491526", "--output", "exportedDocs/testAPI.doc"])
-
-# curl -D- \
-#    -X GET \
-#    -H "Authorization: Basic aGFydmV5aWJleW5vbkBnbWFpbC5jb206a0VuNUFQUEk5ZWhUbDVsZkhSalo1MTM1" \
-#    -H "Content-Type: application/json" \
-#    "https://harveybeynon.atlassian.net/wiki/exportword?pageId=491526" \
-#    --output "exportedDocs/test.doc"
+    subprocess.run(["curl", "-D-", "-X", "GET", "-H", f"Authorization: Basic {API_KEY}", "-H", "Content-Type: application/json", f"https://harveybeynon.atlassian.net/wiki/exportword?pageId={value}", "--output", f"exportedDocs/{pageNum}+{key}.doc"])
+    pageNum += 1
 
 baseDir = 'exportedDocs\\' # Starting directory for directory walk
 
@@ -66,19 +69,8 @@ for dir_path, dirs, files in os.walk(baseDir):
                         print('Failed to Convert: {0}'.format(file_path))
                         print(e)
 
-dir_name = "exportedDocs\\"
-test = os.listdir(dir_name)
-for item in test:
-
-    # Delete the .docx files in the exportedDocs dir
-    if (item.endswith(".docx")):
-        os.remove(os.path.join(dir_name, item))
-
-    # Move all .rst files to the docs/pages dir
-    if (item.endswith(".rst")):
-        src_path = os.path.join("exportedDocs/", item)
-        dst_path = os.path.join("docs/pages/", item)
-        shutil.move(src_path, dst_path)
+delete_files_in_dir("exportedDocs/", ".docx")
+move_files("exportedDocs/", "docs/pages/", ".rst")
 
 # TODO currently this scripts is called from a git bash shell - May need to get the git shh and secret key
 # if this script were to run from a an API call.
